fb322ce007e50c01a38a21bfc5d7f7bf04d7f754
Tyler Finck <hi@tylerfinck.com>
2019-11-07 09:53:04 -0500

    First push
    Just starting, more edits to come.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # upstatedesign
  Unoffical Directory of Upstate New York Design Studios/People
+
+This all stared because Google Search results have become a joke. I was looking for a graphic design firm in Binghamton, less than an hour from where I live, and the results were garbage. So here you go.
+
+_[upstate.design](https://upstate.design)_
+
---
+++ index.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:locale" content="en_US"/>
+<meta property="og:site_name" content="Upstate Design"/>
+<meta property="og:title" content="Upstate Design"/>
+<meta property="og:url" content="https://www.upstate.design"/>
+<meta property="og:type" content="website"/>
+<meta property="og:description" content="Abridged and Unofficial Directory of the “Upstate” New York Graphic Design Community"/>
+<meta property="og:image" content="https://upstate.design/upstate-card.png">
+<meta itemprop="name" content="Upstate Design"/>
+<meta itemprop="headline" content="Upstate Design"/>
+<meta itemprop="description" content="Abridged and Unofficial Directory of the “Upstate” New York Graphic Design Community"/>
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Upstate Design"/>
+<meta name="twitter:url" content="https://www.upstate.design"/>
+<meta name="twitter:description" content="Abridged and Unofficial Directory of the “Upstate” New York Graphic Design Community"/>
+<meta name="twitter:site" content="@typeler"/>
+<meta name="twitter:image" content="https://upstate.design/upstate-card.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<title>Abridged and Unofficial Directory of the “Upstate” New York Graphic Design Community</title>
+<link rel="stylesheet" media="all" href="style.css" />
+</head>
+<body>
+  <h1 id="title">The Abridged and Unofficial Directory of the “Upstate” New York Graphic Design Community</h1>
+  <p id="timecheck">Local Time:
+  <?php $estTime = (new DateTime('America/New_York'))->format('h:i');
+  echo $estTime;
+  ?></p>
+
+<section id="page">
+  <ul>
+  <li><a href="https://76west.agency"><h2>76 West</h2><p><span class="location">📍Syracuse</span> <span class="site">👁76west.agency</span></p></a></li>
+  <li><a href="https://adgcreative.design"><h2>ADG Creative</h2><p><span class="location">📍Albany</span> <span class="site">👁adgcreative.design</span></p></a></li>
+  <li><a href="http://www.weareadjacent.com"><h2>Adjacent</h2><p><span class="location">📍Syracuse</span> <span class="site">👁weareadjacent.com</span></p></a></li>
+  <li><a href="http://www.awpny.com"><h2>AWP</h2><p><span class="location">📍Ithaca</span> <span class="site">👁awpny.com</span></p></a></li>
+  <li><a href="http://beardesign.co/"><h2>Bear Design Co.</h2><p><span class="location">📍Schenectady</span> <span class="site">👁beardesign.co/</span></p></a></li>
+  <li><a href="http://www.bzdesign.com"><h2>BZDesign</h2><p><span class="location">📍Rochester</span> <span class="site">👁bzdesign.com</span></p></a></li>
+  <li><a href="http://www.cinderdesign.co/"><h2>Cinder Design Co</h2><p><span class="location">📍Schenectady</span> <span class="site">👁cinderdesign.co</span></p></a></li>
+  <li><a href="http://www.cobp.com"><h2>CObP</h2><p><span class="location">📍Ithaca</span> <span class="site">👁cobp.com</span></p></a></li>
+  <li><a href="http://www.do-design-creative.com/"><h2>Do Design Creative</h2><p><span class="location">📍Buffalo</span> <span class="site">👁do-design-creative.com</span></p></a></li>
+  <li><a href="http://elevation10k.com"><h2>Elevation 10 Thousand</h2><p><span class="location">📍Latham</span> <span class="site">👁elevation10k.com</span></p></a></li>
+  <li><a href="http://www.etc.supply"><h2>Etcetera Type Co.</h2><p><span class="location">📍Ithaca</span> <span class="site">👁etc.supply</span></p></a></li>
+  <li><a href="http://www.freshysites.com"><h2>Freshy Sites</h2><p><span class="location">📍Binghamton</span> <span class="site">👁freshysites.com</span></p></a></li>
+  <li><a href="https://giansantidesign.com/"><h2>Giansanti Design</h2><p><span class="location">📍Syracuse</span> <span class="site">👁giansantidesign.com</span></p></a></li>
+  <li><a href="https://hanerino.com/"><h2>Hanerino</h2><p><span class="location">📍Rochester</span> <span class="site">👁hanerino.com</span></p></a></li>
+  <li><a href="http://www.irondesign.com"><h2>Iron Design</h2><p><span class="location">📍Ithaca</span> <span class="site">👁irondesign.com</span></p></a></li>
+  <li><a href="http://www.id29.com"><h2>ID29</h2><p><span class="location">📍Troy</span> <span class="site">👁id29.com</span></p></a></li>
+  <li><a href="http://www.idea-kraft.com"><h2>Idea Kraft</h2><p><span class="location">📍Binghamton</span> <span class="site">👁idea-kraft.com</span></p></a></li>
+  <li><a href="http://www.lunchboxbrain.com"><h2>Lunchboxbrain</h2><p><span class="location">📍Rensselaer</span> <span class="site">👁lunchboxbrain.com</span></p></a></li>
+  <li><a href="http://www.tothelab.co"><h2>The Lab Creative</h2><p><span class="location">📍Syracuse</span> <span class="site">👁tothelab.co</span></p></a></li>
+  <li><a href="http://www.minervadesign.com"><h2>Minerva</h2><p><span class="location">📍Rochester</span> <span class="site">👁minervadesign.com</span></p></a></li>
+  <li><a href="http://www.moonfarmer.com"><h2>Moonfarmer</h2><p><span class="location">📍Kingston</span> <span class="site">👁moonfarmer.com</span></p></a></li>
+  <li><a href="https://mpwmarketing.com"><h2>MPW</h2><p><span class="location">📍Clinton</span> <span class="site">👁mpwmarketing.com</span></p></a></li>
+  <li><a href="http://typeworkstudio.com"><h2>OtherWisz Creative</h2><p><span class="location">📍Buffalo</span> <span class="site">👁otherwisz.com</span></p></a></li>
+  <li><a href="http://rtistrybydesign.com"><h2>Rtistry By Design</h2><p><span class="location">📍Binghamton</span> <span class="site">👁rtistrybydesign.com</span></p></a></li>
+  <li><a href="https://www.sidekickcreative.us/"><h2>Sidekick Creative</h2><p><span class="location">📍Glens Falls</span> <span class="site">👁sidekickcreative.us</span></p></a></li>
+  <li><a href="http://spiraldesign.com"><h2>Spiral Design Studio</h2><p><span class="location">📍Cohoes</span> <span class="site">👁spiraldesign.com</span></p></a></li>
+  <li><a href="http://typeworkstudio.com"><h2>Typework Studio</h2><p><span class="location">📍Buffalo</span> <span class="site">👁typeworkstudio.com</span></p></a></li>
+  <li><a href="https://upsidecollective.com"><h2>Upside Collective</h2><p><span class="location">📍Albany</span> <span class="site">👁upsidecollective.com</span></p></a></li>
+
+  <li><a href="https://www.upupcreative.com/"><h2>Up Up Creative</h2><p><span class="location">📍Rochester</span> <span class="site">👁upupcreative.com</span></p></a></li>
+  <li><a href="http://zanzinato.com"><h2>Zanzinato</h2><p><span class="location">📍Ithaca</span> <span class="site">👁zanzinato.com</span></p></a></li>
+  </ul>
+</section>
+<footer id="footer">
+<p>Latest Update: November 2019<br />
+Maintained by: <a href="https://www.madebytyler.com">Tyler</a> via <a href="#">Github</a><br />
+Upstate designer? <a href="#">Submit your info</a></p>
+</footer>
+
+
+
+
+</body>
+</html>
